32187e49f2d2b8063e28430257c712a9ae44f456
yakovBader <yakovbader@edu.aac.ac.il>
2021-05-28 02:06:37 +0300

    middle

--- withData.js
+++ withData.js
@@ -1,5 +1,11 @@
+const { info } = require("node:console");
+
+
+
 //https://${server}.gofile.io/download/${code}/${MD5}/${file name}
 let token='b2af86fc3339ea38e577732b73b1b00ceb7a23ecfc69a89ba722b8a4beaf734e'
+let titles=[];
+let information=[];
 function getServer(e) {
     e.preventDefault();
     let server;
@@ -22,8 +28,13 @@ function postInfo(data,server){
     allfiles[data.fileName] = {
         server: server,
         code:data.code,
-        MD5:data.md5
+        MD5:data.md5,
+        date:form[0],
+        name:form[1],
+        subtitle:form[2],
+        title:"title"
     }
+    allfiles[data.fileName]
 
     
     var raw = JSON.stringify({
@@ -41,7 +52,7 @@ function postInfo(data,server){
         redirect: 'follow'
     };
 
-    fetch("http://192.168.43.229:5000/users/data", requestOptions)
+    fetch("http://capslock-core.herokuapp.com/users/data", requestOptions)
         .then(response => response.json())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
@@ -52,4 +63,37 @@ function getList(){
         .then(response => response.json())
         .then(data=>console.log(data))
         .catch(error=>console.log(error))
+}
+
+function getUsers(){
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("http://capslock-core.herokuapp.com/users/?Accept=application/json&Content-Type=application/json", requestOptions)
+        .then(response => response.json())
+        .then(result => collect(result))
+        .catch(error => console.log('error', error));
+}
+
+function collect(data){
+    for(person in data){
+        let myUserData = person.banjex;
+        for(info in myUserData){
+            information.push(info)
+            tit=info.title;
+            if(myUserData == undefined) {
+                continue;
+            }else{
+                for(let i=0;i<titles.length;i++){
+                    if(tit==titles[i]){
+                        break;
+                    }else if(i==titles.length-1){
+                        titles.push(tit);
+                    }
+                }
+            }
+        } 
+    }
 }
